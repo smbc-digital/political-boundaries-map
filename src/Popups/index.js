6f693a91653f166f4008f53099c2a6dc8973a412
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-02-22 09:21:42 +0000

    Popup Changes

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -21,7 +21,7 @@ const area_committeePopup = (feature, layer) => {
 }
 
 const parliamentaryPopup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-university" aria-hidden="true"></i><p class="title">Parliamentary</p>
+  const content = `<div class="item"><i class="fa fa-address-book-o" aria-hidden="true"></i><p class="title">Parliamentary</p>
   <p class="info">Parliamentary Constituency: ${feature.properties.name_short}</p>
   <p class="info">MP: ${feature.properties.mp_name} (${feature.properties.mp_party})<br> 
   
