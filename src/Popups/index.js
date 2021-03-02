f743f608d0b2cc4d874c6b33df92fcb63dc99d0b
Lydia Gray <lydia.gray@stockport.gov.uk>
2021-03-02 15:28:29 +0000

    fix(project): include DS stylesheet in solution

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -1,30 +1,45 @@
 const wardPopup = (feature, layer) => {
-  const content = `<div class="item"><i class="tag fa fa-map-marker"></i><p class="title">Ward</p>
-  <p class="info">Ward: ${feature.properties.ward_name}</p>
-  <p class="info">Councillors: <br>
-  ${feature.properties.cllr_1} (${feature.properties.cllr_1_party})<br> 
-  ${feature.properties.cllr_2} (${feature.properties.cllr_2_party})<br> 
-  ${feature.properties.cllr_3} (${feature.properties.cllr_3_party}) </p>
-  
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fas fa-map-marker-alt smbc-map__item__header__block__icon"></i>
+      <span class="smbc-map__item__header__block__title">Ward</span>
+    </div>
+    <div class="smbc-map__item__body">
+      <p>Ward: ${feature.properties.ward_name}</p>
+      <p>Councillors: <br>
+      ${feature.properties.cllr_1} (${feature.properties.cllr_1_party})<br> 
+      ${feature.properties.cllr_2} (${feature.properties.cllr_2_party})<br> 
+      ${feature.properties.cllr_3} (${feature.properties.cllr_3_party}) </p>
+    </div>
   </div>`
 
   layer.bindPopup(content)
 }
 
 const area_committeePopup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-university" aria-hidden="true"></i><p class="title">Area Committee</p>
-  <p class="info">Area Committee: ${feature.properties.committee_name}</p>
-  
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="fas fa-university smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Area Committee</span>
+    </div>
+    <div class="smbc-map__item__body">
+      <p>Area Committee: ${feature.properties.committee_name}</p>
+    </div>
   </div>`
 
   layer.bindPopup(content)
 }
 
 const parliamentaryPopup = (feature, layer) => {
-  const content = `<div class="item"><i class="fa fa-address-book-o" aria-hidden="true"></i><p class="title">Parliamentary</p>
-  <p class="info">Parliamentary Constituency: ${feature.properties.name_short}</p>
-  <p class="info">MP: ${feature.properties.mp_name} (${feature.properties.mp_party})<br> 
-  
+  const content = `<div class="smbc-map__item">
+    <div class="smbc-map__item__header__block">
+      <i class="far fa-address-book smbc-map__item__header__block__icon" aria-hidden="true"></i>
+      <span class="smbc-map__item__header__block__title">Parliamentary</span>
+    </div>
+    <div class="smbc-map__item__body">
+      <p>Parliamentary Constituency: ${feature.properties.name_short}</p>
+      <p>MP: ${feature.properties.mp_name} (${feature.properties.mp_party})<br> 
+    </div>
   </div>`
 
   layer.bindPopup(content)
